fd66362893b7a61a0470fc20129effc7ff7a75bb
Enrique Budiono <ebudiono@hawk.iit.edu>
2022-05-12 21:41:06 -0500

    adding index page

---
+++ code/server.js
@@ -0,0 +1,39 @@
+const cookieParser = require('cookie-parser');
+const { response } = require('express');
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+var urlecondedParser = bodyParser.urlencoded({ extended: false });
+
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.set('view engine', 'ejs');
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.static('public'));
+app.use('/static', express.static('public'));
+
+// Database Connection
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'coffeegram',
+    password: 'coffeegram2022',
+    database: 'coffeegram-menu'
+});
+
+app.get('/', (req, res) => {
+    console.log("Entering welcome page");
+    res.render('index');
+});
+
+app.all('*', (req, res) => {
+    console.log("Entering 404 page");
+    res.status(404).send("<h1>404</h1>");
+});
+
+app.listen(PORT,() => {
+    console.log(`Server is running on port ${PORT}`);
+});
