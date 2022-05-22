c7378fd6d064e1c28845d90886cf88275b76b235
Enrique Budiono <ebudiono@hawk.iit.edu>
2022-05-22 16:29:24 -0500

    adding admin panel

--- code/server.js
+++ code/server.js
@@ -28,6 +28,10 @@ app.get('/', (req, res) => {
     res.render('index');
 });
 
+app.get('/admin', (req, res) => {
+    res.render('admin');
+});
+
 app.get('/menu', (req, res) => {
     res.render('menu');
 });
